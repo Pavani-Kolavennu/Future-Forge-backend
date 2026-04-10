c0b03fa5a154ea6562f3083b14a09696d2b23732
“Pavani” <pavanikolavennu16@gmail.com>
2026-04-10 09:10:31 +0530

    Future forge files

--- src/main/java/com/futureforge/auth/AuthRequestDto.java
+++ src/main/java/com/futureforge/auth/AuthRequestDto.java
@@ -1,16 +1,22 @@
 package com.futureforge.auth;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record AuthRequestDto(
-		@JsonAlias({"username", "userEmail"})
-		@Email(message = "Email must be valid")
-		@NotBlank(message = "Email is required")
-		String email,
-		@NotBlank(message = "Password is required")
-		String password
-) {
-}
+        @JsonAlias({"username", "userEmail"})
+        @Email(message = "Email must be valid")
+        @NotBlank(message = "Email is required")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, message = "Password must be at least 8 characters long")
+        @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+            message = "Password must have at least one uppercase letter, one lowercase letter, one number, and one special character"
+        )
+        String password
+) {}
